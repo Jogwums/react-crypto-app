e86a423be8266ec0e1f152bd4ae901b0bfeedfe4
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2020-11-15 10:57:35 +0100

    up1

--- src/components/CryptoCurrency.js
+++ src/components/CryptoCurrency.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import axios from 'axios'
 import Coin from './Coin'
 
@@ -20,9 +20,6 @@ function  CryptoCurrency() {
         setSearch(e.target.value)
     }
 
-    const handleSubmit = () => {
-
-    }
 
     const filteredCrypto = crypto.filter(item => 
         item.name.toLowerCase().includes(search.toLowerCase()))
