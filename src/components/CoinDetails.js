72019593b1633cfcedfbc8cf95a1659ea2a13b7a
Jonathan Ogwumike <jonathanogwumike@gmail.com>
2020-11-16 21:33:24 +0100

    sp1

--- src/components/CoinDetails.js
+++ src/components/CoinDetails.js
@@ -1,21 +1,21 @@
-import React from 'react'
+// import React from 'react'
 
-const CoinDetails = () => {
+// const CoinDetails = () => {
 
-    const [loading, setLoading] = React.useState(false)
+//     const [loading, setLoading] = React.useState(false)
 
-    if(loading){
-        <div className="loader"> 
-            <h2>Loading...</h2>
-        </div>
+//     if(loading){
+//         <div className="loader"> 
+//             <h2>Loading...</h2>
+//         </div>
 
-    }
-    
-    return (
-        <div>
-            More details 
-        </div>
-    )
-}
+//     }
 
-export default CoinDetails
+//     return (
+//         <div>
+//             More details 
+//         </div>
+//     )
+// }
+
+// export default CoinDetails
